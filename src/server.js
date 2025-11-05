89272f585082bb51b2fde374e8478715591e398a
Venkatesh <venkateshkumar.mandla@tekworks.in>
2025-11-05 13:00:54 +0530

    cors issue for mobile

--- src/server.js
+++ src/server.js
@@ -32,7 +32,10 @@ const corsOptions = FRONTEND_URL === '*'
       allowedHeaders: ['Content-Type', 'Authorization']
     }
   : {
-      origin: [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'],
+      origin: [FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174',
+        'capacitor://localhost',
+  'http://localhost',            
+  'http://localhost:5173',      ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization']
